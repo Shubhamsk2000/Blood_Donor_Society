2a3bc5bf35085c58f1c7480260d8a37342134863
Shubham Kondhalkar <shubhamkondhalkar2003@gmail.com>
2023-05-09 16:23:26 +0530

    first commit

---
+++ public/main.js
@@ -0,0 +1,20 @@
+
+let hamMenu = document.getElementsByClassName("menu-container")[0];
+let line1 = document.getElementsByClassName("line")[0];
+let line2 = document.getElementsByClassName("line")[1];
+let line3 = document.getElementsByClassName("line")[2];
+let line = document.getElementsByClassName("line")[0];
+let menu = document.getElementsByClassName("menu-items")[0]
+
+let returnValue;
+
+hamMenu.addEventListener('click', () => {
+    line1.classList.toggle("line-1");
+    line2.classList.toggle("line-3");
+    line3.classList.toggle("line-2");
+
+    returnValue = line.classList.contains("line-1")
+    returnValue === true ? menu.classList.add("transform") : menu.classList.remove("transform")
+
+   
+});
